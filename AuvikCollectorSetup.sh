9356cc34180df9bc1b7e066426e5d499147b6e83
rcdbservices <rcdbservices@gmail.com>
2025-05-15 21:25:54 +0800

    Update AuvikCollectorSetup.sh

--- AuvikCollectorSetup.sh
+++ AuvikCollectorSetup.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-if [[ "$(head -c 1 "$0" | od -An -t uC)" == "13" ]]; then
-  echo "Normalizing this script's line endings..."
-  sed -i 's/\r$//' "$0"
-fi
-
-set -e  # Exit immediately if a command exits with a non-zero status
-
 # Function to display progress
 function notify_step() {
   echo -e "\n========================"
   echo "$1"
   echo "========================"
 }
-
-notify_step "Checking if dos2unix is installed..."
-if ! command -v dos2unix &> /dev/null; then
-  notify_step "dos2unix is not installed. Installing dos2unix..."
-  sudo apt update -y
-  sudo apt install -y dos2unix
-else
-  notify_step "dos2unix is already installed."
-fi
 
 notify_step "Choose what to download:"
 echo "1. AuvikExecutable.sh"
